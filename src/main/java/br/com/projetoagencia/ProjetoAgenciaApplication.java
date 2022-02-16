32d7c580ca95e921cae25443d6aac301176d3ea5
Jonatha Cassio <jonathacassio@gmail.com>
2022-02-16 11:05:35 -0300

    first commit

---
+++ src/main/java/br/com/projetoagencia/ProjetoAgenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoagencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAgenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAgenciaApplication.class, args);
+	}
+
+}
